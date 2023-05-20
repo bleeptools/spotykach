3ec0c6a0b148f837ff95725ed81dc6a49789ebce
vlly <bleeptools@icloud.com>
2023-05-20 22:49:04 +0200

    Sync!

--- control/sync.cpp
+++ control/sync.cpp
@@ -19,23 +19,57 @@ void Sync::run(Core& core) {
 
 void Sync::advance() {
     if (!_is_playing) return;
-    
-    _nticks = (_fticks + kTRtime) / _tempo_mks;
-    _fticks += kTRtime - _nticks * _tempo_mks;
-    auto et = _elapsed_ticks + _nticks;
-    if (et > _elapsed_ticks) {
-        _elapsed_ticks = et;
-        _core->pulse();
+    sync();
+}
+
+void Sync::sync() {
+    uint32_t nticks;
+
+    if (_hold) {
+        nticks = (_fticks + kTRtime) / _tempo_mks;
+        _fticks += kTRtime - (nticks * _tempo_mks);
+        _tempo_ticks += nticks;
+        return;
+    }
+
+    if (_resync) {
+        _fticks = 0;
+        nticks = kTicksPerClock - (_ticks - _ticks_at_last_clock);
+        _ticks_at_last_clock = _ticks + nticks;
+        _tempo_mks -= (kTicksPerClock - _tempo_ticks) * _tempo_mks / kPPQN;
+        _tempo_ticks = 0;
+        _resync = false;
+    }
+    else {
+        nticks = (_fticks + kTRtime) / _tempo_mks;
+        _fticks += kTRtime - (nticks * _tempo_mks);
+        _tempo_ticks += nticks;
+        if (_ticks - _ticks_at_last_clock + nticks >= kTicksPerClock) {
+            nticks = kTicksPerClock - 1 - (_ticks - _ticks_at_last_clock);
+            _hold = true;
+        }
+    }
+
+    if (nticks > 0) {
+        _ticks += nticks;
+        for (uint32_t i = 0; i < nticks; i++) {
+            _core->pulse();
+        }
     }
 }
 
 void Sync::set_is_playing(bool is_playing) {
     if (is_playing != _is_playing) {
-        _nticks = 0;
-        _fticks = 0;
-        _elapsed_ticks = 0;
+        reset();    
     }
     _is_playing = is_playing;
+}
+
+void Sync::set_tempo(float norm_value) {
+    if (fcomp(norm_value, _raw_tempo)) return;
+    _raw_tempo = norm_value;
+    _tempo = (kTempoMax - kTempoMin) * norm_value + kTempoMin;
+    _tempo_mks = tempo_mks(_tempo);
 }
 
 void Sync::pull(daisy::DaisySeed& hw) {
@@ -46,73 +80,18 @@ void Sync::pull(daisy::DaisySeed& hw) {
     _last_state = new_state;
 }
 
-float Sync::tempo() {
-    return _tempo;
-}
-
-void Sync::set_tempo(float norm_value) {
-    if (fcomp(norm_value, _raw_tempo)) return;
-    _raw_tempo = norm_value;
-    _tempo = (kTempoMax - kTempoMin) * norm_value + kTempoMin;
-    _tempo_mks = tempo_mks(_tempo);
+void Sync::clock_in_tick() {
+    if (!_is_playing) return;
+    _resync = true;
+    _hold = false;
+    sync();
 }
 
 void Sync::reset() {
-    _ptime = 0;
-    _wndw.fill(0);
-    _iterator = 0;
-    _filled = false;
-    _dev_cnt = 0;
-}
-
-void Sync::clock_in_tick() {
-    auto t = daisy::System::GetNow();
-    auto delta = t - _ptime;
-    if (_ptime > 0) {
-        push(delta);
-        _tempo = std::min(tempo(avg()), kTempoMax);
-        _tempo_mks = tempo_mks(_tempo);
-        checkDeviation(delta);
-    }
-    _ptime = t;
-
-    // _beat.ticks++;
-    // if (_beat.ticks == kPPQN) {
-    //     _beat.ticks = 0;
-    //     _beat.beats ++;
-    // }
-    // _core->pulse();
-}
-
-void Sync::push(uint32_t interval) {
-    _wndw[_iterator] = interval;
-    _iterator ++;
-    _iterator %= _wndw.size(); 
-    if (_iterator == 0) {
-        _filled = true;
-    }
-}
-
-float Sync::avg() {
-    float s = 0;
-    auto l = _filled ? _wndw.size() : _iterator;
-    for (size_t i = 0; i < l; i++) s += _wndw[i];
-    _avg = s / l;
-    return _avg;
-}
-
-float Sync::tempo(float tick) {
-    return round(15000.f / tick);
-}
-
-void Sync::checkDeviation(uint32_t delta) {
-    if (delta > _avg - _dev_thres && delta < _avg + _dev_thres) {
-        _dev_cnt = 0;
-        return;
-    }
-    if (++_dev_cnt < _dev_cnt_thres) return;
-    
-    _iterator = 0;
-    _filled = false;
-    _dev_cnt = 0;
+    _fticks = 0;
+    _ticks = 0;
+    _ticks_at_last_clock = 0;
+    _tempo_ticks = 0;
+    _resync = false;
+    _hold = false;
 }
--- control/sync.h
+++ control/sync.h
@@ -30,48 +30,42 @@ public:
     void run(Core& core);
     void advance();
     void pull(daisy::DaisySeed& hw);
-    float tempo();
+    
+    float tempo() { return 60000000.f / _tempo_mks; }
     void set_tempo(float normValue);
+
     void set_is_playing(bool is_playing);
 
 private:
-    inline uint32_t tempo_mks(const float tempo) {
+    void clock_in_tick();
+    void sync();
+    void reset();
+    uint32_t tempo_mks(const float tempo) {
         return static_cast<uint32_t>(kSecondsPerMinute * 1e6 / tempo);
     }
 
     Core* _core;
-
-    bool _filled = false;
-    uint32_t _ptime = 0;
-    std::size_t _iterator = 0;
-    std::array<uint32_t, 16> _wndw; //4 ticks * 4 beats = 1 measure
-    uint8_t _dev_cnt = 0; //deviations count
-    uint8_t _dev_cnt_thres = 3; //deviations until reset
-    uint32_t _dev_thres = 3; //min deviation to consider
+    daisy::GPIO g;
 
     bool _is_playing = false;
 
     const uint32_t kInterval = 1e6 * kBufferSize / kSampleRate;
     const uint32_t kTRtime = kPPQN * kInterval;
+    const uint32_t kTicksPerClock = kPPQN / 4;
     uint32_t _tempo_mks = tempo_mks(_tempo);
+    uint32_t _tempo_delta_mks = 0;
+    uint32_t _ticks = 0;
     uint32_t _fticks = 0;
-    uint32_t _nticks = 0;
-    uint32_t _elapsed_ticks = 0;
+    uint32_t _ticks_at_last_clock = 0;
+    uint32_t _tempo_ticks = 0;
 
-    float _avg = 125;
+    bool _hold = false;
+    bool _resync = false;
+
+    Beat _beat;
     float _tempo = 120;
     float _raw_tempo = _tempo;
-    Beat _beat;
     int _last_state = 1;
-    
-    daisy::GPIO g;
-
-    void clock_in_tick();
-    void push(uint32_t interval);
-    float avg();
-    float tempo(float tick);
-    void checkDeviation(uint32_t delta);
-    void reset();
 };
 
 }
