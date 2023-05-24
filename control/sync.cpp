68921efa9aaadf7a35e3812966261e36af67b09c
vlly <bleeptools@icloud.com>
2023-05-24 14:08:53 +0200

    Comment

--- control/sync.cpp
+++ control/sync.cpp
@@ -17,6 +17,7 @@ void Sync::run(Core& core) {
 	g.Init(cfg);
 }
 
+// Called by internal interrupt timer (audio callback in this implementation).
 void Sync::tick() {
     if (!_is_playing) return;
     sync();
@@ -24,10 +25,23 @@ void Sync::tick() {
 
 /*
 Derived from Maximum MIDI Music Applications in C++ by Paul Messick
+This method generates internal ticks and also synchronises to the external clock.
+So it's called both from internal interrupt timer (audio callback in this implementation) and upon external clock tick reception.
+nticks - integer count of internal ticks
+_fticks - fractional count of internal ticks
+_tempo_ticks - integer ticks count since last external clock
+_tempo_mks - tempo in microseconds / beat (quarter note)
+_resync - flag to resync to external clock. Is set to true once external clock tick is received.
+_hold - flag to stop advancing internal timeline if the number of internal ticks exceeded expected count of internal ticks per extrnal tick
+kTRTime - internal resolution (ppqn) multiplied by interrupt interval.
 */
 void Sync::sync() {
     uint32_t nticks = 0;
 
+    //If we generated more internal ticks per extrnal tick as expected,
+    //we don't advance internal "timeline", but only accumulate _tempo_ticks
+    //in order to calculate and correct the tempo.
+    //This flag is set to false upon reception of the external tick.
     if (_hold) {
         nticks = (_fticks + kTRtime) / _tempo_mks;
         _fticks += kTRtime - (nticks * _tempo_mks);
@@ -35,6 +49,9 @@ void Sync::sync() {
         return;
     }
 
+    //Once a tick of the extrnal clock is received,
+    //we do resync, i.e. align inernal timeline with the external one
+    //and adjust tempo.
     if (_resync) {
         _fticks = 0;
         nticks = kTicksPerClock - (_ticks - _ticks_at_last_clock);
@@ -43,10 +60,14 @@ void Sync::sync() {
         _tempo_ticks = 0;
         _resync = false;
     }
+    //Regular mode. We generate internal ticks between external ticks.
     else {
         nticks = (_fticks + kTRtime) / _tempo_mks;
         _fticks += kTRtime - nticks * _tempo_mks;
         _tempo_ticks += nticks;
+        //If there are more internal ticks per the external tick than 
+        //expected, we set _hold to true effectively stopping advancing timeline
+        //until next external tick
         if (_ticks - _ticks_at_last_clock + nticks >= kTicksPerClock) {
             nticks = kTicksPerClock - 1 - (_ticks - _ticks_at_last_clock);
             _hold = true;
@@ -59,6 +80,12 @@ void Sync::sync() {
     }
 }
 
+/*
+In case of external clock sync this
+method only schedules playback. Actual playback 
+starts on the first tick of the external clock.
+see clock_in_tick() below.
+*/
 void Sync::set_is_playing(bool is_playing) {
     if (is_playing == _is_playing) return;
      
@@ -71,6 +98,10 @@ void Sync::set_is_playing(bool is_playing) {
     }
 }
 
+/*
+Setting tempo from internal control. 
+Has no effect in case of syncing to extrnal clock.
+*/
 void Sync::set_tempo(float norm_value) {
     if (fcomp(norm_value, _raw_tempo)) return;
     _raw_tempo = norm_value;
@@ -78,6 +109,9 @@ void Sync::set_tempo(float norm_value) {
     _tempo_mks = tempo_mks(_tempo);
 }
 
+/*
+Read external clock pin
+*/
 void Sync::pull(daisy::DaisySeed& hw) {
     auto new_state = g.Read();
     if (_last_state && !new_state) {
@@ -86,6 +120,12 @@ void Sync::pull(daisy::DaisySeed& hw) {
     _last_state = new_state;
 }
 
+/*
+External clock received
+This method starts playback on first received clock
+after playback was scheduled. After that, calls sync 
+for every tick received.
+*/
 void Sync::clock_in_tick() {
     if (!_is_playing && !_is_about_to_play) return;
 
