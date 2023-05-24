4a081686a0c84556996d9951b13119f6a9ec448f
vlly <bleeptools@icloud.com>
2023-05-24 22:35:26 +0200

    More steps

--- core/globals.h
+++ core/globals.h
@@ -37,15 +37,17 @@ namespace spotykach {
     static const uint32_t kPPQN             { 240 };
 
     using Step = int;
-    static constexpr std::array<Step, 9> EvenSteps {{
+    static constexpr std::array<Step, 11> EvenSteps {{
         4 * kPPQN,      //1
         3 * kPPQN,      //1 / 2.
         2 * kPPQN,      //1 / 2
         3 * kPPQN / 2,  //1 / 4.
+        2 * kPPQN / 3,  //1 / 4T
         1 * kPPQN,      //1 / 4
         1 * kPPQN / 2,  //1 / 8
         1 * kPPQN / 3,  //1 / 8T
         1 * kPPQN / 4,  //1 / 16
+        1 * kPPQN / 6,  //1 / 16T
         1 * kPPQN / 8,  //1 / 32
     }};
 
